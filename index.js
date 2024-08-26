cecc77b7b410540017be7f22a0687e938f03aa39
Gurehmat <gurehmat9@gmail.com>
2024-08-26 13:26:22 -0400

    form javascript

---
+++ index.js
@@ -0,0 +1,29 @@
+// template_8v88oe2
+// service_ratktue
+// 2JvjrKrWx-SLTLM7O
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_ratktue",
+        "template_8v88oe2",
+        event.target,
+        "2JvjrKrWx-SLTLM7O"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on gurehmat9@gmail.com"
+        );
+      });
+  }
+  
