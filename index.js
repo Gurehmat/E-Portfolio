d401f76ac68e48cb7e1028abc18085dd3dd0d124
Gurehmat <gurehmat9@gmail.com>
2024-08-28 01:39:05 -0400

    final commit without footer fix

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-// template_8v88oe2
-// service_ratktue
-// 2JvjrKrWx-SLTLM7O
 
 let isModalOpen =false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+  }
+}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
